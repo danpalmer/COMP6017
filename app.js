f383314e8fd86189c5ce481ee7ab4970119c82dc
Dan Palmer <dan.palmer@me.com>
2013-11-23 17:25:42 +0000

    Express project setup

--- app.js
+++ app.js
@@ -8,20 +8,28 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var orm = require('orm');
+var models = require('./models');
+var validate = require('express-validator');
+
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(orm.express('sqlite://database.db', models));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.bodyParser());
+app.use(validate());
+app.use(function(req, res, next) {
+  req.uri = req.protocol + '://' + req.get('host') + req.url;
+  next();
+});
+
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -29,7 +37,14 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+// app.get('/index.:format?', routes.index);
+
+// app.get('/collections.:format?', collections.list);
+// app.post('/collections.:format?', collections.create);
+
+// app.get('/collections/:id.:format?', collections.collection);
+// app.get('/collections/:cid/images.:format?', images.list);
+// app.get('/collections/:cid/images/:id.:format?', images.image);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
