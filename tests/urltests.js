7bd055974d0e52faaf43acf70df79f360977b006
Sam Bull <sambull91@gmail.com>
2013-11-23 19:18:50 +0000

    Added tests for /question/:id, /question/:id/comment

--- tests/urltests.js
+++ tests/urltests.js
@@ -3,6 +3,7 @@ var expect = require('expect.js');
 var host = 'localhost:3000';
 
 describe('/question', function(){
+
 	it('server should respond', function(done){
 		request.get(host + '/question', function(error, response){
 			expect(response).to.not.be(undefined);
@@ -10,12 +11,14 @@ describe('/question', function(){
 			done();
 		});
 	});
+	
 	it('should return 200 for GET', function(done){
 		request.get(host + '/question', function(error, response){
 			expect(response.statusCode).to.be(200);
 			done();
 		});
 	});
+	
 	it('should return 201 created for POST', function(done){
 		//needs author key added to form
 		request.post(host + '/question', {form:{title:'foo', content:'bar?'}}, function(error, response){
@@ -42,7 +45,115 @@ describe('/question', function(){
 	});
 	*/
 });
-/*
+
 describe('/question/:id', function(){
 
-});*/
+	it('server should respond', function(done){
+		request.get(host + '/question/:id', function(error, response){
+			expect(response).to.not.be(undefined);
+			expect(response).to.not.be(null);
+			done();
+		});
+	});
+	
+	it('should return 200 for GET', function(done){
+		request.get(host + '/question/:id', function(error, response){
+			expect(response.statusCode).to.be(200);
+			done();
+		});
+	});
+	
+	it('should return 201 created for POST', function(done){
+		//needs author key added to form
+		request.post(host + '/question/:id', {form:{title:'foo', content:'bar?'}}, function(error, response){
+			expect(response.statusCode).to.be(201);
+			done();
+		});
+	});
+	
+	it('should return a valid question', function(done){
+		request.get({url:host + '/question/:id', json:true}, function(error, response, body){
+			expect(body['content']).to.not.be(null);
+			done();
+		});
+	});
+	/*
+	Need author key in POSTed form before test can be done
+	it('should have a user', function(done){
+	});
+	*/
+	
+	/*
+	Need author key in POSTed form before test can be done
+	it('should link to a valid user', function(done){	
+	});
+	*/
+
+	it('server should respond', function(done){
+		request.head(host + '/question/:id', function(error, response){
+			expect(response).to.not.be(undefined);
+			expect(response).to.not.be(null);
+			done();
+		});
+	});
+	
+	it('should return 200 for GET', function(done){
+		request.head(host + '/question/:id', function(error, response){
+			expect(response.statusCode).to.be(200);
+			done();
+		});
+	});
+	
+	it('should return 404 for GET after DELETE', function(done){
+		request.del(host + '/question/:id', function(error, response){
+			request.get(host + 'question/:id', function(error, response){
+				expect(response.statusCode).to.be(404);
+				done();			
+			}
+		});
+	});
+});
+
+describe('/question/:id/comment', function(){
+
+	it('server should respond', function(done){
+		request.get(host + '/question/:id/comment', function(error, response){
+			expect(response).to.not.be(undefined);
+			expect(response).to.not.be(null);
+			done();
+		});
+	});
+	
+	it('should return 200 for GET', function(done){
+		request.get(host + '/question/:id/comment', function(error, response){
+			expect(response.statusCode).to.be(200);
+			done();
+		});
+	});
+	
+	it('should return 201 created for POST', function(done){
+		//needs author key added to form
+		request.post(host + '/question/:id/comment', {form:{title:'foo', content:'bar?'}}, function(error, response){
+			expect(response.statusCode).to.be(201);
+			done();
+		});
+	});
+	
+	it('should return a valid comment', function(done){
+		request.get({url:host + '/question/:id/comment', json:true}, function(error, response, body){
+			expect(body['content']).to.not.be(null);
+			done();
+		});
+	});
+	/*
+	Need author key in POSTed form before test can be done
+	it('should have a user', function(done){
+	});
+	*/
+	
+	/*
+	Need author key in POSTed form before test can be done
+	it('should link to a valid user', function(done){	
+	});
+	*/
+});
