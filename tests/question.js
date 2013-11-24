1d2a869c611fae3a05ade3b27815ff06da080986
Dan Palmer <dan.palmer@me.com>
2013-11-24 19:14:28 +0000

    Fix Question tests, now all working, although more need to be added.

--- tests/question.js
+++ tests/question.js
@@ -4,46 +4,46 @@ var util = require('./util');
 
 var host = util.host;
 
-describe('/question', function(){
+describe('/question', function() {
 
-  it('server should respond', function(done){
-    request.get(host + '/question', function(error, response){
+  it('server should respond', function(done) {
+    request.get(host + '/question', function(error, response) {
       expect(response).to.not.be(undefined);
       expect(response).to.not.be(null);
       done();
     });
   });
   
-  it('should return 200 for GET', function(done){
-    request.get(host + '/question', function(error, response){
+  it('should return 200 for GET', function(done) {
+    request.get(host + '/question', function(error, response) {
       expect(response.statusCode).to.be(200);
       done();
     });
   });
   
-  it('should return 201 created for POST', function(done){
+  it('should return 201 created for POST', function(done) {
     //needs author key added to form
-    request.post(host + '/question', {form:{title:'foo', content:'bar?'}}, function(error, response){
+    request.post(host + '/question', {form:{title:'foo', content:'bar?'}}, function(error, response) {
       expect(response.statusCode).to.be(201);
       done();
     });
   });
   
-  it('should return a valid question', function(done){
-    request.get({url:host + '/question/:id', json:true}, function(error, response, body){
+  it('should return a valid question', function(done) {
+    request.get({url:host + '/question/:id', json:true}, function(error, response, body) {
       expect(body['content']).to.not.be(null);
       done();
     });
   });
   /*
   Need author key in POSTed form before test can be done
-  it('should have a user', function(done){
+  it('should have a user', function(done) {
   });
   */
   
   /*
   Need author key in POSTed form before test can be done
-  it('should link to a valid user', function(done){ 
+  it('should link to a valid user', function(done) { 
   });
   */
 
@@ -51,69 +51,101 @@ describe('/question', function(){
   // POST with missing values returns 400 bad request
 });
 
-describe('/question/:id', function(){
+describe('/question/:id', function() {
 
-  it('server should respond', function(done){
-    request.get(host + '/question/:id', function(error, response){
-      expect(response).to.not.be(undefined);
-      expect(response).to.not.be(null);
-      done();
+  it('server should respond', function(done) {
+    util.createUser(function(user) {
+      util.createQuestion(user.id, function(question) {
+        request.get(host + '/question/' + question.id, function(error, response) {
+          expect(response).to.not.be(undefined);
+          expect(response).to.not.be(null);
+          done();
+        });
+      })
     });
   });
   
-  it('should return 200 for GET', function(done){
-    request.get(host + '/question/:id', function(error, response){
-      expect(response.statusCode).to.be(200);
-      done();
+  it('should return 200 for GET', function(done) {
+    util.createUser(function(user) {
+      util.createQuestion(user.id, function(question) {
+        request.get(host + '/question/' + question.id, function(error, response) {
+          expect(response.statusCode).to.be(200);
+          done();
+        });
+      })
     });
   });
   
-  it('should return 201 created for POST', function(done){
-    //needs author key added to form
-    request.post(host + '/question/:id', {form:{title:'foo', content:'bar?'}}, function(error, response){
-      expect(response.statusCode).to.be(201);
-      done();
+  it('should return 201 created for POST', function(done) {
+    var title = 'title';
+    var content = 'content';
+    util.createUser(function(user) {
+      request.post({
+        url: host + '/question',
+        json: true,
+        form: {
+          title: title,
+          content: content,
+          author_id: user.id
+        }
+      }, function(error, response) {
+        expect(response.statusCode).to.be(201);
+        done();
+      });
     });
   });
   
-  it('should return a valid question', function(done){
-    request.get({url:host + '/question/:id', json:true}, function(error, response, body){
+  it('should return a valid question', function(done) {
+    request.get({url:host + '/question/:id', json:true}, function(error, response, body) {
       expect(body['content']).to.not.be(null);
       done();
     });
   });
   /*
   Need author key in POSTed form before test can be done
-  it('should have a user', function(done){
+  it('should have a user', function(done) {
   });
   */
   
   /*
   Need author key in POSTed form before test can be done
-  it('should link to a valid user', function(done){ 
+  it('should link to a valid user', function(done) { 
   });
   */
 
-  it('server should respond for HEAD', function(done){
-    request.head(host + '/question/:id', function(error, response){
-      expect(response).to.not.be(undefined);
-      expect(response).to.not.be(null);
-      done();
+  it('server should respond for HEAD', function(done) {
+    util.createUser(function(user) {
+      util.createQuestion(user.id, function(question) {
+        request.head(host + '/question/' + question.id, function(error, response) {
+          expect(response).to.not.be(undefined);
+          expect(response).to.not.be(null);
+          done();
+        });
+      })
     });
   });
   
-  it('should return 200 for HEAD', function(done){
-    request.head(host + '/question/:id', function(error, response){
-      expect(response.statusCode).to.be(200);
-      done();
+  it('should return 200 for HEAD', function(done) {
+    util.createUser(function(user) {
+      util.createQuestion(user.id, function(question) {
+        request.head(host + '/question/' + question.id, function(error, response) {
+          expect(response.statusCode).to.be(200);
+          done();
+        });
+      })
     });
   });
   
-  it('should return 404 for GET after DELETE', function(done){
-    request.del(host + '/question/:id', function(error, response){
-      request.get(host + '/question/:id', function(error, response){
-        expect(response.statusCode).to.be(404);
-        done();     
+  it('should return 404 for GET after DELETE', function(done) {
+    util.createUser(function(user) {
+      util.createQuestion(user.id, function(question) {
+        request.del(host + '/question/' + question.id, function(deleteError, deleteResponse) {
+          expect(deleteResponse.statusCode).to.be(204);
+          request.get(host + '/question/' + question.id, function(checkError, checkResponse) {
+            expect(checkResponse.statusCode).to.be(404);
+            done();
+          });
+        });
       });
     });
   });
