0961c419396dea0a5d4c4ba6388a0a7ce4ae0986
Dan Palmer <dan.palmer@me.com>
2013-12-16 21:17:35 +0000

    Added new lines and headers

--- curlscript.sh
+++ curlscript.sh
@@ -7,45 +7,45 @@ answer='/answer'
 comment='/comment'
 
 ## Create a user
-echo -e "\nCreate a user"
-curl --data "name=foo&email=foo@bar.com" --request POST $url$user
+echo -e "\n\nCreate a user\n"
+curl --data "name=foo&email=foo@bar.com" --request POST $url$user -i
 
 ## Get list of users
-echo -e "\nGet list of users"
-curl --request GET $url$user
+echo -e "\n\nGet list of users\n"
+curl --request GET $url$user -i
 
 ## Get a user
-echo -e "\nGet a single user"
-curl --request GET "$url$user/1"
+echo -e "\n\nGet a single user\n"
+curl --request GET "$url$user/1" -i
 
 ## Edit the user
-echo -e "\nEdit the same user"
-curl --data "name=edited&email=bar@baz.com" --request PUT "$url$user/1"
+echo -e "\n\nEdit the same user\n"
+curl --data "name=edited&email=bar@baz.com" --request PUT "$url$user/1" -i
 
 ## Create a question
-echo -e "\nCreate a question"
-curl --data "content=foo?&author_id=1&title=question" --request POST $url$question
+echo -e "\n\nCreate a question\n"
+curl --data "content=foo?&author_id=1&title=question" --request POST $url$question -i
 
 ## Get the a list of questions
-echo -e "\nGet list of questions"
-curl --request GET $url$question
+echo -e "\n\nGet list of questions\n"
+curl --request GET $url$question -i
 
 ## Get a question
-echo -e "\nGet a single question"
-curl --request GET "$url$question/1"
+echo -e "\n\nGet a single question\n"
+curl --request GET "$url$question/1" -i
 
 ## Edit the question
-echo -e "\nEdit the question"
-curl --data "content=bar?&author_id=1&title=editedquestion" --request PUT "$url$question/1"
+echo -e "\n\nEdit the question\n"
+curl --data "content=bar?&author_id=1&title=editedquestion" --request PUT "$url$question/1" -i
 
 ## Answer the question
-echo -e "\nAnswer the question"
-curl --data "content=answer&author_id=1" --request POST "$url$question/1$answer"
+echo -e "\n\nAnswer the question\n"
+curl --data "content=answer&author_id=1" --request POST "$url$question/1$answer" -i
 
 ## Comment on the question
-echo -e "\nComment on the question"
-curl --data "content=questioncomment&author_id=1" --request POST "$url$question/1$comment"
+echo -e "\n\nComment on the question\n"
+curl --data "content=questioncomment&author_id=1" --request POST "$url$question/1$comment" -i
 
 ## Comment on the answer
-echo -e "\nComment on the answer"
-curl --data "content=answercomment&author_id=1" --request POST "$url$question/1$answer/1$comment"
+echo -e "\n\nComment on the answer\n"
+curl --data "content=answercomment&author_id=1" --request POST "$url$question/1$answer/1$comment" -i
